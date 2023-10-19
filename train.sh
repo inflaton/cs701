ab10ca4e283edd5c4fff7de72e93db10e02eb2fc
Donghao Huang <Donghao.Huang@mastercard.com>
2023-10-20 06:05:59 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -13,14 +13,14 @@ grep MemTotal /proc/meminfo
 EPOCHS=20
 BATCH=32
 
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=1
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=2
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=3
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=4
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=5
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=6
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=7
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=8
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=9
-python train.py --epochs=$EPOCHS --batch=$BATCH --phase=10
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=1
+python train.py --epochs=$EPOCHS --batch=$BATCH --phase=2 --checkpoint=7
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=3
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=4
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=5
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=6
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=7
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=8
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=9
+# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=10
 

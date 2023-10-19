1385202dfca4e31418bd3931cc01b1255a26e3be
Donghao Huang <Donghao.Huang@mastercard.com>
2023-10-20 06:23:25 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -15,8 +15,8 @@ BATCH=32
 # python train.py --epochs=20 --batch=$BATCH --phase=1
 # python train.py --epochs=20 --batch=$BATCH --phase=2 --checkpoint=7
 # python train.py --epochs=30 --batch=$BATCH --phase=3 --checkpoint=15
-python train.py --epochs=40 --batch=$BATCH --phase=4 --checkpoint=21
-# python train.py --epochs=$EPOCHS --batch=$BATCH --phase=5
+# python train.py --epochs=40 --batch=$BATCH --phase=4 --checkpoint=21
+python train.py --epochs=40 --batch=$BATCH --phase=5 --checkpoint=16
 # python train.py --epochs=$EPOCHS --batch=$BATCH --phase=6
 # python train.py --epochs=$EPOCHS --batch=$BATCH --phase=7
 # python train.py --epochs=$EPOCHS --batch=$BATCH --phase=8
